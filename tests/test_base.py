d893434fe4de077842efd047d8c1022c1d77e3bc
simon0820s <simon0820s@gmail.com>
2022-12-28 21:37:34 -0500

    blueprints

--- tests/test_base.py
+++ tests/test_base.py
@@ -17,7 +17,34 @@ class MainTest(TestCase):
     def test_app_in_test_mode(self):
         self.assertTrue(current_app.config['TESTING'])
 
+    def test_index_redirects(self):
+        response = self.client.get(url_for('index'))
+
+        self.assertRedirects(response, url_for('hello'))
+
     def test_hello_get(self):
         response = self.client.get(url_for('hello'))
 
         self.assert200(response)
+
+    def test_hello_post(self):
+        fake_form = {
+            'username': 'fake',
+            'password': 'fake-password'
+        }
+        response = self.client.post(url_for('hello'), data=fake_form)
+
+        self.assertRedirects(response, url_for('index'))
+
+    def test_auth_blueprint_exists(self):
+        self.assertIn('auth', self.app.blueprints)
+
+    def test_auth_login_get(self):
+        response = self.client.get(url_for('auth.login'))
+
+        self.assert200(response)
+
+    def test_auth_login_template(self):
+        self.client.get(url_for('auth.login'))
+
+        self.assertTemplateUsed('login.html')
